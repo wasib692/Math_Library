a60be2a77e37209d86a3b688b424c09d782c543a
Wasib Sattar <2017ee22@student.uet.edu.pk>
2021-12-10 15:28:17 +0500

    3rd

--- Mathlibrary/src/main/java/com/appsol360/mathlibrary/Arithmatic.java
+++ Mathlibrary/src/main/java/com/appsol360/mathlibrary/Arithmatic.java
@@ -1,20 +1,20 @@
 package com.appsol360.mathlibrary;
 
 public class Arithmatic {
-    public static float Sum(double a, double b){
-        double c=a+b;
-        return (float) c;
+    public static double Sum(double a, double b){
+
+        return  a+b;
     }
-    public static float Subtract(double a, double b){
-        double c=a-b;
-        return (float) c;
+    public static double Subtract(double a, double b){
+
+        return  a-b;
     }
-    public static float Divide(double a, double b){
-        double c=a/b;
-        return (float) c;
+    public static double Divide(double a, double b){
+
+        return  a/b;
     }
-    public static float Multiply(double a, double b){
-        double c=a*b;
-        return (float) c;
+    public static double Multiply(double a, double b){
+
+        return a*b;
     }
 }
