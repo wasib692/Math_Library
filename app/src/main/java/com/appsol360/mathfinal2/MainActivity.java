19df80d5e8b82d6478fdb10e0235c6c24fdbd478
Wasib Sattar <2017ee22@student.uet.edu.pk>
2021-12-14 15:17:00 +0500

    6

--- app/src/main/java/com/appsol360/mathfinal2/MainActivity.java
+++ app/src/main/java/com/appsol360/mathfinal2/MainActivity.java
@@ -3,12 +3,25 @@ package com.appsol360.mathfinal2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.appsol360.mathlibrary.Arithmatic;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.bclick_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Arithmatic.calculator(MainActivity.this);
+            }
+        });
+
     }
 }
